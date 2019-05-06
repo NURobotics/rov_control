366e200e43c8d196b86c3a2be4a77ed1893ae738
Yehya Elmasry <yehya_elmasry20@yahoo.com>
2019-05-05 23:15:56 -0500

    Add interface with ROS joy package

--- src/joystick_server.cpp
+++ src/joystick_server.cpp
@@ -1,18 +1,23 @@
 #include "ros/ros.h"
 #include "joystick.h"
+#include <sensor_msgs/Joy.h>
+
+void joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
+    ROS_INFO("Axis 0 is %f", joy->axes[0]);
+}
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "joystick_server");
 
   ros::NodeHandle n;
+  ros::Subscriber joystick_sub = n.subscribe<sensor_msgs::Joy>("joy", 1, &joyCallback);
 
   ros::Rate loop_rate(10);
-
+  
+  ROS_INFO("Hello World!");
   while (ros::ok())
   {
-    ROS_INFO("Hello World!");
-
     ros::spinOnce();
 
     loop_rate.sleep();
